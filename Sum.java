c666c2144009611b418ffc739d60b81f2c7581bd
shivrastogi01 <76581550+shivrastogi01@users.noreply.github.com>
2022-12-26 10:52:51 +0530

    Add files via upload

---
+++ Sum.java
@@ -0,0 +1,14 @@
+public class Sum
+{
+public static void main(String[] a)
+	{
+int j=0;
+for(int i=1;i<=10;i++)
+		{
+//j = j+i;
+j+=i;
+System.out.println("Current no is = "+i );
+		}
+System.out.println("Total = " +j);
+	}
+}
